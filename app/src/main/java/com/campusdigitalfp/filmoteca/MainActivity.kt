ab9539caa3e1ebf1e7940a3bd12c984e97d2ba66
Cgarces <carlos.garces@gmail.com>
2025-01-12 21:23:29 +0100

    Ejercicio 6. Añadiendo contenido a la pantalla

--- app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/MainActivity.kt
@@ -1,47 +1,92 @@
 package com.campusdigitalfp.filmoteca
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.campusdigitalfp.filmoteca.ui.theme.FilmotecaTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FilmotecaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AboutScreen(author = "Carlos Garcés")
                 }
+            }
+        }
+
+@Composable
+fun AboutScreen(author: String) {
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement  = Arrangement.Center) {
+            Text(
+                text = "Creada por: $author",
+                fontSize = MaterialTheme.typography.titleLarge.fontSize, // Tamaño de la fuente.
+                fontWeight = FontWeight.Bold, // Estilo de la fuente en negrita.
+                textAlign = TextAlign.Center, // Alineación del texto en el centro.
+                modifier = Modifier
+                    .fillMaxWidth() // Ocupa todo el ancho disponible.
+                    .padding(16.dp) // Aplica un padding de 16 dp alrededor del texto.
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.perfil),
+                contentDescription = "Perfil desarrollador",
+                modifier = Modifier
+                    .size(150.dp)
+            )
+
+            Row(horizontalArrangement = Arrangement.SpaceEvenly) {
+                Button(onClick = {
+                    showToast(
+                        context,
+                        message = "Funcionalidad sin implementar"
+                    )
+                }) {
+                    Text("Ir al sitio web")
+                }
+                Button(onClick = {
+                    showToast(
+                        context,
+                        message = "Funcionalidad sin implementar"
+                    )
+                }) {
+                    Text("Obtener soporte")
+                }
+            }
+            Button(onClick = { showToast(context, message = "Funcionalidad sin implementar") }) {
+                Text("Volver")
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FilmotecaTheme {
-        Greeting("Android")
-    }
+fun showToast(context: Context, message: String) {
+    Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
