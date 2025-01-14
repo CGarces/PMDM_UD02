45da96e533919e8f9b8b8584c0e45538ec4d033b
Cgarces <carlos.garces@gmail.com>
2025-01-14 01:53:54 +0100

    Ejercicio 11. Resultado de la pantalla FilmEditScreen

--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmDataScreen.kt
@@ -1,26 +1,25 @@
 package com.campusdigitalfp.filmoteca.screens
 
-import androidx.compose.foundation.Image
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.R
@@ -28,6 +27,15 @@ import com.campusdigitalfp.filmoteca.R
 @Composable
 fun FilmDataScreen(navController: NavHostController, pelicula: String)   {
     val context = LocalContext.current
+    val savedStateHandle = navController.currentBackStackEntry?.savedStateHandle
+    if (savedStateHandle != null) {
+        val result by savedStateHandle.getLiveData<String>("result").observeAsState()
+        LaunchedEffect(result) {
+            if (result != null)
+                Toast.makeText(context, result, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -47,7 +55,9 @@ fun FilmDataScreen(navController: NavHostController, pelicula: String)   {
             Button(onClick = { navController.navigate("view/Dummy") }) {
                 Text(stringResource(R.string.ver_relacion))
             }
-            Button(onClick = { navController.navigate("edit") }) {
+            Button(onClick = {
+                navController.navigate("edit")
+            }) {
                 Text(stringResource(R.string.editar))
             }
             Button(onClick = { navController.popBackStack("list", false) }) {
--- app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmEditScreen.kt
+++ app/src/main/java/com/campusdigitalfp/filmoteca/screens/FilmEditScreen.kt
@@ -1,26 +1,20 @@
 package com.campusdigitalfp.filmoteca.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.campusdigitalfp.filmoteca.R
@@ -41,10 +35,16 @@ fun FilmEditScreen(navController: NavHostController)   {
                     .fillMaxWidth() // Ocupa todo el ancho disponible.
                     .padding(16.dp) // Aplica un padding de 16 dp alrededor del texto.
             )
-            Button(onClick = { navController.popBackStack() }) {
+            Button(onClick = {
+                navController.previousBackStackEntry?.savedStateHandle?.set("result", "RESULT_OK")
+                navController.popBackStack()
+            }) {
                 Text(stringResource(R.string.guardar))
             }
-            Button(onClick = { navController.popBackStack() }) {
+            Button(onClick = {
+                navController.previousBackStackEntry?.savedStateHandle?.set("result", "RESULT_CANCELED")
+                navController.popBackStack()
+            }) {
                 Text(stringResource(R.string.cancelar))
             }
         }
